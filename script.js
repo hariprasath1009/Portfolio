b9d686d0ecb0cdeb34fdd0dada516c0c236bfc8d
Hariprasath <hariprasath2899@gmail.com>
2024-11-17 18:44:10 +0530

    files upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function togglefunction(){
+    const menu = document.querySelector(".menu-links");
+    const icon=document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
